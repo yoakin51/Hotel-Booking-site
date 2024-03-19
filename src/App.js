9bf0ae00c6d163df558bf1399cff686fd91bd39a
Yoakin Mesfin <ymesfin807@gmail.com>
2024-03-19 03:59:45 +0300

    Hotel booking system frontend first draft

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import Navbar from "./common/Navbar/Navbar"
+import Home from "./components/pages/Home"
+import About from "./components/about/About"
+import Gallery from "./components/gallery/Gallery"
+//import Destinations from "./Components/Destinations/Destinations"
+//import DHome from "./Components/Destinations/Home"
+import Destinations from "./components/Destinations/Home"
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import SinglePage from "./SinglePage/SinglePage"
+
+/*-------------blog------------ */
+import Blog from "./components/Blog/Blog"
+import BlogSingle from "./components/Blog/blog-single-page/BlogSingle"
+import Testimonial from "./components/Testimonial/Testimonial"
+import Contact from "./components/Contact/Contact"
+import Footer from "./common/footer/Footer"
+import Login from "./components/login/Login"
+import Register from "./components/login/Register"
+/*-------------blog------------ */
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/about' exact component={About} />
+          <Route path='/gallery' exact component={Gallery} />
+          <Route path='/destinations' exact component={Destinations} />
+          <Route path='/singlepage/:id' component={SinglePage} />
+          <Route path='/blog' exact component={Blog} />
+          <Route path='/blogsingle/:id' component={BlogSingle} />
+          <Route path='/testimonial' component={Testimonial} />
+          <Route path='/contact' component={Contact} />
+          <Route path='/sign-in' component={Login} />
+          <Route path='/Register' component={Register} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
+  )
 }
 
-export default App;
+export default App
